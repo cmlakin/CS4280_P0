d714a729ec3c5b5a3cf7a5170b40b36c8d8fb090
Corrina Lakin <corrina.lakin@gmail.com>
2022-02-12 20:35:21 -0600

    printing and formatting working

--- traversals.h
+++ traversals.h
@@ -1,10 +1,11 @@
 #include <fstream>
 
-using std::fstream;
+using std::ofstream;
 
-void preOrder(node_t*, fstream&);
-void postOrder(node_t*);
-void printLevelOrder(node_t*);
-void printCurrentLevel(node_t*, int);
+void preOrder(node_t*, ofstream&);
+void printPostOrder(node_t*, ofstream&);
+void postOrder(node_t*, ofstream&);
+void printLevelOrder(node_t*, ofstream&);
+void printCurrentLevel(node_t*, int, ofstream&);
 int height(node_t*);
 //node_t* newnode_t(int);
